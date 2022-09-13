cd6df1e74cbdf6ef4099522b04bc95f76eec7593
Gabriele Torre <gabriele.torre@kellify.com>
2022-09-13 18:23:29 +0200

    first commit

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sentinel_data_manager',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
