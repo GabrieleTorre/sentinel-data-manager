4d124d75b1ef5f430b82a009c69348c47386e2b0
GabrieleTorre <usefmddj@gmail.com>
2022-10-28 08:55:04 +0000

    added features to sentinel 2 data manager

--- src/sentinel_2.py
+++ src/sentinel_2.py
@@ -15,7 +15,8 @@ class Indices:
 
     def get_valid_data(self, x):
         imgs = np.load(x['data'])
-        mask = (np.load(x['masks']) < self.prob_thresh).any(axis=0)
+        mask = np.load(x['masks']).sum(axis=0)
+        mask = (mask < self.prob_thresh)
         return imgs.astype(np.float64), mask
 
     def ndvi(self, x):
@@ -58,22 +59,24 @@ class Indices:
 
 class data_manager(Indices):
 
-    def __init__(self, source_dir, prob_thresh=40, img_thresh=0.6):
+    def __init__(self, source_dir, prob_thresh=5, img_thresh=0.95):
 
-        self.prob_thresh = prob_thresh  # mask threshold probability level
-        self.img_thresh = img_thresh  # max percentage of masked pixels allowed
+        self.prob_thresh = prob_thresh # mask threshold probability level
+        self.img_thresh = img_thresh   # max percentage of masked pixels allowed
 
         bands = ["B02", "B03", "B04", "B05", "B06",
                  "B07", "B08", "B8A", "B11", "B12"]
         self.bands = {b: i for i, b in enumerate(bands)}
-
+        
         self.data_fnames = glob(join(source_dir, '20*.npy'))
         self.mask_fnames = glob(join(*[source_dir, 'cloudProb', '20*.npy']))
-        self.segm_fname  = glob(join(*[source_dir, 'crops_segmentation.npy']))[0]
-
+        
+        self.sem_fname  = glob(join(*[source_dir, 'crops_semantic.npy']))[0]
+        self.pan_fname  = glob(join(*[source_dir, 'crops_panoptic.npy']))[0]
+        
         self.df_records = self.__get_df__()
-        valid_selection = (self.validate_cloud_level()) & \
-                          (self.validate_snow_level())
+
+        valid_selection = self.validate_cloud_snow_coverage()
         self.df_records = self.df_records[valid_selection]
 
     def __get_df_from_filelist__(self, x):
@@ -89,31 +92,38 @@ class data_manager(Indices):
 
 
     def __validate_weather__(self, im):
-        return (im < self.prob_thresh).mean() > self.img_thresh
+        return (im < self.prob_thresh).mean() >= self.img_thresh
 
-    def validate_cloud_level(self):
+    def validate_cloud_snow_coverage(self):
         _funct_ = lambda x: self.__validate_weather__(np.load(x)[0])
-        return self.df_records.masks.apply(_funct_)
-
-    def validate_snow_level(self):
-        _funct_ = lambda x: self.__validate_weather__(np.load(x)[1])
         return self.df_records.masks.apply(_funct_)
 
     def get_data(self):
         return np.stack([np.load(x) for x in self.data_fnames], axis=0)
 
-    def get_index_ts(self, function, veg_type=None):
+    def descrive_veg_type_(self, seg_type='semantic'):
+        if seg_type == 'semantic':
+            mask = np.load(self.sem_fname)[0]
+        elif seg_type == 'panoptic':
+            mask = np.load(self.pan_fname)[0]
+        values, counts = np.unique(mask, return_counts=True)
+        out =  pd.Series(index=values.astype(int), 
+                         data=counts/np.product(mask.shape))
+        return out[out.index.isin([0, 19]) == False]
+    
+    def get_index_ts(self, function, veg_type=None, seg_type='semantic'):
         index, values = [], []
-        veg_mask = np.load(self.segm_fname)
-
+        veg_mask = np.load(self.sem_fname if seg_type == 'semantic' else self.pan_fname)
+            
         for i, row in self.df_records.iterrows():
 
             VI, MK = function(row)
             if veg_type:
-                VMK = (veg_mask == veg_type)
+                VMK = (veg_mask[0] == veg_type)
             else:
-                VMK = (veg_mask != 0) & (veg_mask != 19)
+                VMK = (veg_mask[0] != 0) & (veg_mask[0] != 19)
             npost = np.where((MK & VMK).squeeze())
+
             values.append(np.median(VI[npost]))
             index.append(i)
 
